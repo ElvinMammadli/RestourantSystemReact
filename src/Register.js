df41a159f48575e4192045a59d8c7db009f89877
ElvinMammadli <az.elvinmammadli@gmail.com>
2021-08-19 19:02:49 +0300

    Register and Login done

--- src/Register.js
+++ src/Register.js
@@ -1,40 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const LoginPage = props => {
+const Login = props => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
-  const [error, setError] = useState();
-}
+  const [passwordAgain, setPasswordAgain] = useState();
 
-function Register() {
+  const [error, setError] = useState();
+
+ 
+
+  const onClickLogin = async event => {
+
+    
+    setError(undefined);
+
+    const { history } = props;
+    const { push } = history;
+
+
+    event.preventDefault();
+    const creds = {
+      username,
+      password
+    };
+    if(password!=null&&passwordAgain!=null&&password.length>6){
+      if(password===passwordAgain){
+        axios({
+          method: 'post',
+          url: 'http://localhost:8080/api/1/user',
+          data: creds,
+          config: { headers: {'Content-Type': 'multipart/form-data' }}
+          })
+          .then(function (response) {
+              push('/User');
+
+              console.log(response.data);
+          })
+          .catch(function (error) {
+              //handle error
+              setError(error.response.data.message);
+              console.log(error.response.data);
+          });
+      }else{
+        setError("Your passwords dont match");
+        
+      }
+  }else {
+    setError("Your password must be grater than 6");
+  }
+}
+  
   return (
+    
     <div>
-      <h1>Register Page to Restourant System</h1>
+      <h1>Register Page to Consumer</h1>
       <form>
+      
         <label>
           UserName:
-          <input type="text" name="username" />
+          <input type="text" name="username" onChange={event => setUsername(event.target.value)} />
         </label>
         <br />
         <label>
           Password:
-          <input type="text" name="password" />
+          <input type="password" name="password"  onChange={event => setPassword(event.target.value)} />
         </label>
-
         <br />
         <label>
           Password Again:
-          <input type="text" name="passwordAgain" />
+          <input type="password" name="passwordAgain"  onChange={event => setPasswordAgain(event.target.value)} />
         </label>
-        <br />
-        <a href="/">Back To Login Page</a>
-        &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-        <input type="submit" value="Register" />
-
-
+        {error && <div className="alert alert-danger">{error}</div>}
+        <button onClick={onClickLogin}  >Register</button>
+        
       </form>
     </div>
   );
-}
+};
 
-export default Register;
+export default Login;
